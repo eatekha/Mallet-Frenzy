0fe68f272733b2e1cd9a70d6235133e11a1e5bff
Anubhav Aery <aaery3@uwo.ca>
2023-11-07 17:35:40 -0500

    Implemented RasberryPi Hardware GPIO pins to the code

--- WacAMole.cpp
+++ WacAMole.cpp
@@ -6,6 +6,14 @@
 #include <map>
 #include <string>
 #include <fstream>
+#include <pigpio.h>
+#include <ncurses.h>
+#include <unordered_map>
+#include <stdexcept>
+#include <random>
+#include <chrono>
+#include <thread>
+#include <string>
 
 using namespace std;
 class Timer {
@@ -54,10 +62,19 @@ private:
 public:
     LEDMatrix() : leds(16) {}
 
-    LED& lightUpRandomLED() {
-        int index = rand() % leds.size();
-        leds[index].turnOn();
-        return leds[index];
+    int lightUpRandomLED(int currentLedPin, std::unordered_map<char, int>& keyToLedMap) {
+        // Random number generator setup
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> distrib(0, keyToLedMap.size() - 1);
+        int randomIndex = distrib(gen);
+        for (auto &led : keyToLedMap) {
+            if (randomIndex-- == 0) {
+                currentLedPin = led.second;
+                break;
+            }
+        }
+        return currentLedPin;
     }
 };
 
@@ -88,7 +105,7 @@ private:
 
 public:
     void print() {
-        std::ifstream inputFile("../highScores.txt");
+        std::ifstream inputFile("highScores.txt");
         //Iterates through file under schema "number score"
         if (inputFile.is_open()) {
             while (inputFile >> name >> score) {
@@ -113,7 +130,7 @@ public:
     //adding to file
     void add(int score, std::string playerName) {
         // Open the file for appending
-        std::ofstream outputFile("../highScores.txt", std::ios::app);
+        std::ofstream outputFile("highScores.txt", std::ios::app);
         if (outputFile.is_open()) {
             // Write new data to the file
             outputFile << std::endl << playerName << " " << score;
@@ -137,6 +154,113 @@ public:
         std::cout << "Game started!\n";
         timer.start();
     }
+    
+    void setup(std::unordered_map<char, int>& keyToLedMap) {
+        if (gpioInitialise() < 0) {
+            throw std::runtime_error("Failed to initialize pigpio.");
+        }
+
+        for (auto const& kvp : keyToLedMap) {
+            gpioSetMode(kvp.second, PI_OUTPUT);
+            gpioWrite(kvp.second, 0); // Initially turn off all LEDs
+        }
+    }
+    
+    void inGame(Player& player, HighScore& highScore, std::unordered_map<char, int>& keyToLedMap)
+    {
+        // Game logic
+        // Initialize ncurses mode
+        initscr();
+        echo(); // Enable echoing of characters
+        printw("Enter your username: ");
+        char str[100];
+        getnstr(str, 99); // Read the username from the user
+        player.setName(str);
+        noecho(); // Disable echoing of characters for the game
+        clear(); // Clear the screen before starting the game
+
+        try {
+            setup(keyToLedMap);
+            cbreak();
+            keypad(stdscr, TRUE); // Enables keypad mode to capture keypresses
+            nodelay(stdscr, TRUE); // Non-blocking getch call
+            curs_set(0); // Hide cursor
+
+            auto startTime = std::chrono::system_clock::now();
+            auto endTime = startTime + std::chrono::seconds(30);
+            int timeLeft = static_cast<int>(std::chrono::duration_cast<std::chrono::seconds>(endTime - startTime).count());
+            bool ledOn = false;
+            int currentLedPin = -1;
+            int score = 0; // Initialize score
+            
+            // Main game loop
+            while (true) {
+                auto currentTime = std::chrono::system_clock::now();
+                timeLeft = static_cast<int>(std::chrono::duration_cast<std::chrono::seconds>(endTime - currentTime).count());
+
+                // Break the loop if the time is up
+                if (timeLeft < 0) {
+                    break;
+                }
+
+                // Display time left and score
+                mvprintw(0, 0, "Time left: %d seconds ", timeLeft);
+                mvprintw(1, 0, "Score: %d", score);
+                refresh();
+
+                if (!ledOn) {
+                    // Turn off the previous LED
+                    if (currentLedPin != -1) {
+                        gpioWrite(currentLedPin, 0);
+                    }
+                    
+                    // Turn on a new random LED
+                    currentLedPin = ledMatrix.lightUpRandomLED(currentLedPin,keyToLedMap);
+                    gpioWrite(currentLedPin, 1);
+                    ledOn = true;
+                }
+
+                int ch = getch();
+                if (ch != ERR) {
+                    if (keyToLedMap.count(ch) > 0) {
+                        if (keyToLedMap[ch] == currentLedPin) {
+                            gpioWrite(currentLedPin, 0); // Turn off the LED
+                            ledOn = false;
+                            score++; // Increase score for a correct hit
+                        } else {
+                            score = std::max(0, score - 1); // Decrement score for an incorrect hit without going below zero
+                        }
+                    }
+                }
+
+                // Sleep for a short time to maintain responsiveness
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            }
+
+            // Game over, clean up
+            for (auto const& kvp : keyToLedMap) {
+                gpioWrite(kvp.second, 0);
+            }
+            gpioTerminate();
+
+            // Display the final score
+            clear();
+            mvprintw(0, 0, "Game Over! %s, your score is: %d", str, score);
+            refresh();
+            nodelay(stdscr, FALSE); // Set getch to blocking mode
+            getch(); // Wait for user input before exiting
+            endwin();
+            
+            // After ending ncurses mode, print the final score and username to the console
+            std::cout << "Game Over! " << str << ", your final score is: " << score << std::endl;
+            highScore.add(score, str);
+            
+        } catch (const std::runtime_error& e) {
+            std::cerr << e.what() << std::endl;
+            endwin();
+            gpioTerminate();
+        }
+    }
 
     void endGame() {
         std::cout << "Game ended!\n";
@@ -151,6 +275,45 @@ public:
         return currentScore;
     }
 };
+
+// Define the pin numbers
+const int led1 = 15;
+const int led2 = 24;
+const int led3 = 8;
+const int led4 = 20;
+const int led5 = 14;
+const int led6 = 23;
+const int led7 = 7;
+const int led8 = 21;
+const int led9 = 3;
+const int led10 = 17;
+const int led11 = 5;
+const int led12 = 19;
+const int led13 = 2;
+const int led14 = 27;
+const int led15 = 6;
+const int led16 = 26;
+
+// Map the keys to corresponding LED pins
+std::unordered_map<char, int> keyToLedMap = {
+    {'4', led1},
+    {'5', led2},
+    {'6', led3},
+    {'7', led4},
+    {'r', led5},
+    {'t', led6},
+    {'y', led7},
+    {'u', led8},
+    {'f', led9},
+    {'g', led10},
+    {'h', led11},
+    {'j', led12},
+    {'v', led13},
+    {'b', led14},
+    {'n', led15},
+    {'m', led16}
+};
+
 
 int main() {
     HighScore highScore;
@@ -172,14 +335,8 @@ int main() {
 
         switch (choice) {
             case 1:
-                cout << "Enter player name: ";
-                cin >> playerName;
-                player.setName(playerName);
-
                 game.startGame();
-
-                // Game logic goes here
-                highScore.add(game.getScore(), playerName);
+                game.inGame(player, highScore, keyToLedMap);
                 game.endGame();
                 break;
             case 2:
